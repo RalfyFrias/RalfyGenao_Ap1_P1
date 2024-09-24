234867fa6b27bf62ba901775d355e1cb33bdf7c8
ralfy <ralfy@RALFY>
2024-09-23 22:05:04 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+using Microsoft.EntityFrameworkCore;
+using RalfyGenao_Ap1_P1.Components;
+using RalfyGenao_Ap1_P1.DAL;
+
+namespace RalfyGenao_Ap1_P1
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var builder = WebApplication.CreateBuilder(args);
+
+			// Add services to the container.
+			builder.Services.AddRazorComponents()
+				.AddInteractiveServerComponents();
+
+			var ConStr = builder.Configuration.GetConnectionString("ConStr");
+			builder.Services.AddDbContext<Contexto>(Options => Options.UseSqlite(ConStr));
+
+
+			var app = builder.Build();
+
+			// Configure the HTTP request pipeline.
+			if (!app.Environment.IsDevelopment())
+			{
+				app.UseExceptionHandler("/Error");
+				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+				app.UseHsts();
+			}
+
+			app.UseHttpsRedirection();
+
+			app.UseStaticFiles();
+			app.UseAntiforgery();
+
+			app.MapRazorComponents<App>()
+				.AddInteractiveServerRenderMode();
+
+			app.Run();
+		}
+	}
+}
