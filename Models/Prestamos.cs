2ab3a06073ad174409208807705f3b7573533a6e
ralfy <ralfy@RALFY>
2024-09-30 22:32:19 -0300

    crando la nueva variable y clase del modelo

---
+++ Models/Prestamos.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RalfyGenao_Ap1_P1.Models
+{
+	public class Prestamo
+	{
+		[Key]
+
+		public int PrestamoId { get; set; }
+		[Required(ErrorMessage = "Campo obligatorio")]
+		public string? Deudor { get; set; }
+        [Required(ErrorMessage = "Campo obligatorio")]
+        public string? Concepto { get; set; }
+        [Required(ErrorMessage = "Campo obligatorio")]
+        public decimal? Monto { get; set; }
+
+	}
+}
