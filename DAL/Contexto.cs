f6b3037f3a99e27dfb3d79b110d3fbd84be17d31
ralfy <ralfy@RALFY>
2024-10-12 23:17:59 -0300

    modificando el contexto

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -5,12 +5,23 @@ namespace RalfyGenao_Ap1_P1.DAL
 {
 	public class Contexto : DbContext
 	{
-		public Contexto(DbContextOptions<Contexto> options)
-				 : base(options) { }
-		public DbSet<Prestamos> Prestamos { get; set; }
-        public DbSet<Deudor> Duedor { get; set; }
-
+        public Contexto(DbContextOptions<Contexto> options)
+    : base(options) { }
+        public DbSet<Prestamos> Prestamos { get; set; }
+        public DbSet<Deudor> Deudor { get; set; }
         public DbSet<Cobros> Cobros { get; set; }
-
+        public DbSet<CobroDetalle> cobroDetalle { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Deudor>().HasData(new List<Deudor>()
+        {
+            new Deudor() {DeudorId=1, Nombres="Ralfy"},
+             new Deudor() {DeudorId=2, Nombres="Joshua"},
+              new Deudor() {DeudorId=3, Nombres="Juanes"},
+               new Deudor() {DeudorId=4, Nombres="eddy"},
+                new Deudor() {DeudorId=5, Nombres="Penda"}
+        });
+        }
     }
 }
